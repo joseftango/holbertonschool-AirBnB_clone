c5af3ac3ec965a75f31e657fb1e5d4d96e2c7fe2
joseftango <4279@holbertonschool.com>
2025-01-10 13:18:20 +0100

    model

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''base_model module'''
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    '''BaseModel class'''
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''returns a string representation of the object'''
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        '''save method'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''converts the instance to dictionary'''
+        my_dict = {}
+        my_dict['__class__'] = self.__class__.__name__
+
+        for k, v in self.__dict__.items():
+            if k == 'created_at' or k == 'updated_at':
+                my_dict[k] = v.isoformat()
+            else:
+                my_dict[k] = v
+
+        return my_dict
+
+
