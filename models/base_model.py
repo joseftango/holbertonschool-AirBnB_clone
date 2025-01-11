edadc878fe081a8d65d1985b65a9bc6134ba5699
joseftango <4279@holbertonschool.com>
2025-01-11 13:15:51 +0100

    models

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         '''returns a string representation of the object'''
@@ -28,7 +29,6 @@ class BaseModel:
     def save(self):
         '''save method'''
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
