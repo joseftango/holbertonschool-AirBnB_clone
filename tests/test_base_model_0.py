bb53a0ba914c1c8a1bbcd607f0e6cd2d6a7e8406
joseftango <4279@holbertonschool.com>
2025-01-11 03:22:05 +0100

    test_base_model_0.py

--- tests/test_base_model_0.py
+++ tests/test_base_model_0.py
@@ -7,16 +7,12 @@ from datetime import datetime
 
 class Test_BaseModel(unittest.TestCase):
 
-    def test_Base_module_attributes(self):
-        b = base_model.BaseModel()
-        self.assertIsNotNone(b)
-        self.assertIsInstance(b.id, str, "ID is not a string")
-
-        uuid_obj = uuid.UUID(b.id)
-        self.assertIsInstance(uuid_obj, uuid.UUID, "Generated object is not a UUID instance")
-        self.assertEqual(uuid_obj.version, 4, "UUID version is not 4")
-
-        self.assertIsInstance(b.created_at, datetime, "Generated object is not a datetime instance")
+    def test_save_method(self):
+        model = base_model.BaseModel()
+        updated_at_before_save = model.updated_at
+        self.assertEqual(updated_at_before_save, model.updated_at)
+        model.save()
+        self.assertNotEqual(updated_at_before_save, model.updated_at)
 
     def test_to_dict_method(self):
         model = base_model.BaseModel()
@@ -28,12 +24,16 @@ class Test_BaseModel(unittest.TestCase):
                          "updated_at in dict does not match ISO format")
         self.assertEqual(model_dict["__class__"], 'BaseModel')
 
-    def test_save_method(self):
-        model = base_model.BaseModel()
-        updated_at_before_save = model.updated_at
-        self.assertEqual(updated_at_before_save, model.updated_at)
-        model.save()
-        self.assertNotEqual(updated_at_before_save, model.updated_at)
+    def test_Base_module_attributes(self):
+        b = base_model.BaseModel()
+        self.assertIsNotNone(b)
+        self.assertIsInstance(b.id, str, "ID is not a string")
+
+        uuid_obj = uuid.UUID(b.id)
+        self.assertIsInstance(uuid_obj, uuid.UUID, "Generated object is not a UUID instance")
+        self.assertEqual(uuid_obj.version, 4, "UUID version is not 4")
+
+        self.assertIsInstance(b.created_at, datetime, "Generated object is not a datetime instance")
 
     def test_str_method(self):
         model = base_model.BaseModel()
