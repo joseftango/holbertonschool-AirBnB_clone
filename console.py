274df809abcd98f8f6f62c07cdc0950a816f32b8
joseftango <4279@holbertonschool.com>
2025-01-13 02:02:32 +0100

    console.py

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''console module'''
 from cmd import Cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(Cmd):
@@ -9,17 +10,11 @@ class HBNBCommand(Cmd):
 
     def do_quit(self, arg):
         '''Quit command to exit the program'''
-        print('', end='')
         return True
 
     def do_EOF(self, arg):
         '''EOF command to exit the program'''
-        print('', end='')
         return True
-
-    def emptyline(self):
-        """empty line + ENTER shouldn't execute anything"""
-        pass
 
 
 if __name__ == '__main__':
