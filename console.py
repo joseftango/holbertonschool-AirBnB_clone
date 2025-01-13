2b0ee1acf6053600729b7b84bb671faca2d66120
joseftango <4279@holbertonschool.com>
2025-01-13 02:06:37 +0100

    console.py

--- console.py
+++ console.py
@@ -16,6 +16,9 @@ class HBNBCommand(Cmd):
         '''EOF command to exit the program'''
         return True
 
+    def emptyline(self):
+        '''empty line + ENTER shouldn't execute anything'''
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
